4b505f27ffadd206d726ba27c47905484a0b4b4a
Sun694 <9062244+Sun694@users.noreply.github.com>
2020-09-27 19:05:14 -0500

    change error in comment / code

--- src/transformers/tokenization_mbart.py
+++ src/transformers/tokenization_mbart.py
@@ -212,5 +212,5 @@ class MBartTokenizer(XLMRobertaTokenizer):
     def set_tgt_lang_special_tokens(self, lang: str) -> None:
         """Reset the special tokens to the target language setting. Prefix [tgt_lang_code], suffix =[eos]."""
         self.cur_lang_code = self.lang_code_to_id[lang]
-        self.prefix_tokens = []
-        self.suffix_tokens = [self.eos_token_id, self.cur_lang_code]
+        self.prefix_tokens = [self.cur_lang_code]
+        self.suffix_tokens = [self.eos_token_id]
