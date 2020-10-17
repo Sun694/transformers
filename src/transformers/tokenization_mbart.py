e8d44bbadce831cafa5bddc0abdfa6b4c24af37e
Sun694 <9062244+Sun694@users.noreply.github.com>
2020-10-17 16:30:44 -0500

    Update tokenization_mbart.py

--- src/transformers/tokenization_mbart.py
+++ src/transformers/tokenization_mbart.py
@@ -206,8 +206,8 @@ class MBartTokenizer(XLMRobertaTokenizer):
     def set_src_lang_special_tokens(self, src_lang) -> None:
         """Reset the special tokens to the source lang setting. No prefix and suffix=[eos, cur_lang_code]."""
         self.cur_lang_code = self.lang_code_to_id[src_lang]
-        self.prefix_tokens = []
-        self.suffix_tokens = [self.eos_token_id, self.cur_lang_code]
+        self.prefix_tokens = [self.cur_lang_code]
+        self.suffix_tokens = []
 
     def set_tgt_lang_special_tokens(self, lang: str) -> None:
         """Reset the special tokens to the target language setting. Prefix [tgt_lang_code], suffix =[eos]."""
